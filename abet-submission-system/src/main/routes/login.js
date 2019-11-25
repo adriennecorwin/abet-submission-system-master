762b5df6b247fad3468406d6fb5fba7ec9b17c6c
Sarah Sbert <smsbert@gmail.com>
2019-11-24 22:34:32 -0500

    Task 1 update

--- abet-submission-system/src/main/routes/login.js
+++ abet-submission-system/src/main/routes/login.js
@@ -14,7 +14,7 @@ router.get('/', function (req, res, next) {
 /* POST login page */
 router.post('/', (req, res, next) => {
 	// hard coded username and password
-	if (req.body.username === 'user' && req.body.password === 'password') { //&& await user_lib.is_whitelisted(req.body.username)) {
+	if (req.body.username === 'user' && req.body.password === 'password' && await user_lib.is_whitelisted(req.body.username)) {
 		res.redirect(302, '/course/')
 	} else {
 		res.redirect(302, '/login/')
