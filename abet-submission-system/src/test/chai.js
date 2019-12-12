d98f7ad5c33c86e40b42639b69031365b6142042
Sarah Sbert <smsbert@gmail.com>
2019-12-12 14:24:00 -0500

    fix chai.js

--- abet-submission-system/src/test/chai.js
+++ abet-submission-system/src/test/chai.js
@@ -1,9 +1,5 @@
-// load objection
-require("../main/common/objection")
-
-// setup testing classes
-const chai_as_promised = require("chai-as-promised");
+const chai_as_promised = require('chai-as-promised')
 const chai = require('chai')
-chai.use(chai_as_promised);
+chai.use(chai_as_promised)
 
 module.exports = chai
